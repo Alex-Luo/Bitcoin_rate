26bb03c4722606c31f78177d429b4e0d9d7c9df6
Alex-Luo <allu6345@colorado.edu>
2016-03-04 14:04:52 -0700

    made it heroku compatible

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/', function (req, res) {
   return;
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
