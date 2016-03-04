e1a6b219b8a961d4ee28a8a95d578a95a443b899
Alex-Luo <allu6345@colorado.edu>
2016-03-04 14:10:47 -0700

    took out local host

--- index.js
+++ index.js
@@ -24,21 +24,6 @@ function getUSD(callback) {
 
 app.get('/calculate', function (req, res) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-
-
   getUSD(function(data){
     res.send(data.toString());
   });
